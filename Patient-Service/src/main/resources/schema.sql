8918982cce2e1536880c0e85574691eddda686d9
Shiru99 <shriram.ghadge1999@gmail.com>
2022-08-18 23:29:22 +0530

    Added DB data

--- Patient-Service/src/main/resources/schema.sql
+++ Patient-Service/src/main/resources/schema.sql
@@ -3,8 +3,8 @@ SET DateStyle TO European;
 DROP TABLE IF EXISTS `Users`, `Patients`, `Doctors`,`DoctorDetails`, `Appointments`;
 
 CREATE TABLE `Users` (
-  `mobile_num` varchar(255) PRIMARY KEY NOT NULL,
-  `password` varchar(255) NOT NULL
+  `mobile_num` varchar(255) PRIMARY KEY,
+  `password` varchar(255)
 );
 
 CREATE TABLE `Patients` (
@@ -15,16 +15,16 @@ CREATE TABLE `Patients` (
 CREATE TABLE `Doctors` (
   `mobile_num` varchar(255) PRIMARY KEY,
   `full_name` varchar(255),
-  `department` varchar(255),
+  `department` ENUM ('Cardiology', 'Neurology', 'Orthopedics', 'Gastroenterology', 'Dermatology', 'Urology', 'Oncology', 'Radiology'),
   `degree` varchar(255),
   `profile_URL` varchar(255)
 );
 
 CREATE TABLE `DoctorDetails` (
   `doctor_id` varchar(255) PRIMARY KEY,
-  `education` varchar(255),
-  `work_experience` varchar(255),
-  `specialisation` varchar(255)
+  `education` varchar(1000),
+  `work_experience` varchar(1000),
+  `interest` varchar(255)
 );
 
 CREATE TABLE `Appointments` (
@@ -34,19 +34,19 @@ CREATE TABLE `Appointments` (
   `date` date,
   `slot` ENUM ('Morning1', 'Morning2', 'Morning3', 'Afternoon1', 'Afternoon2', 'Afternoon3', 'Evening1', 'Evening2', 'Evening3'),
   `status` ENUM ('Done', 'Active', 'Cancelled'),
-  `message` varchar(255)
+  `message` varchar(1000)
 );
 
-ALTER TABLE `Patients` ADD FOREIGN KEY (`mobile_num`) REFERENCES `Users` (`mobile_num`);
+ALTER TABLE `Patients` ADD FOREIGN KEY (`mobile_num`) REFERENCES `Users` (`mobile_num`) ON DELETE CASCADE;
 
 
-ALTER TABLE `Doctors` ADD FOREIGN KEY (`mobile_num`) REFERENCES `Users` (`mobile_num`);
+ALTER TABLE `Doctors` ADD FOREIGN KEY (`mobile_num`) REFERENCES `Users` (`mobile_num`) ON DELETE CASCADE;
 
 
-ALTER TABLE `DoctorDetails` ADD FOREIGN KEY (`doctor_id`) REFERENCES `Doctors` (`mobile_num`);
+ALTER TABLE `DoctorDetails` ADD FOREIGN KEY (`doctor_id`) REFERENCES `Doctors` (`mobile_num`) ON DELETE CASCADE;
 
 
-ALTER TABLE `Appointments` ADD FOREIGN KEY (`doctor_id`) REFERENCES `Doctors` (`mobile_num`);
+ALTER TABLE `Appointments` ADD FOREIGN KEY (`doctor_id`) REFERENCES `Doctors` (`mobile_num`)  ON DELETE CASCADE;
 
 
-ALTER TABLE `Appointments` ADD FOREIGN KEY (`patient_id`) REFERENCES `Patients` (`mobile_num`);
+ALTER TABLE `Appointments` ADD FOREIGN KEY (`patient_id`) REFERENCES `Patients` (`mobile_num`)  ON DELETE CASCADE;
